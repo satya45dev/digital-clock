c7dd4433858d04ba48cfe6190108df09450e5012
satya45dev <satya2004mondal@gmail.com>
2026-04-10 19:22:01 +0530

    Add files via upload

---
+++ Digital-Clock/DigitalClockApplication.java
@@ -0,0 +1,13 @@
+package com.digital_clock.Digital.Clock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalClockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalClockApplication.class, args);
+	}
+
+}
