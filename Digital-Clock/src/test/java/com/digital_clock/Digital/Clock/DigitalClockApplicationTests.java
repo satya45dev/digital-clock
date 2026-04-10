968d061a15cd029527f04f8263c66f00b75fd09a
satya45dev <satya2004mondal@gmail.com>
2026-04-10 19:15:45 +0530

    Add files via upload

---
+++ Digital-Clock/src/test/java/com/digital_clock/Digital/Clock/DigitalClockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital_clock.Digital.Clock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalClockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
